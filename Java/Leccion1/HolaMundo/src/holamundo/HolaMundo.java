bd59639f1fd4391985de66e160a801364fb19dc7
Andres Montes <m.andresmontes@gmail.com>
2022-04-26 19:44:54 -0300

    añado readme

---
+++ Java/Leccion1/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package holamundo;
+
+public class HolaMundo {
+    public static  void main (String[] args){
+        System.out.println ( "Hola mundo ");
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        
+        
+    }
+    
+}
